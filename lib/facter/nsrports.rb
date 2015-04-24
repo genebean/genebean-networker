28049740550f51836d7ce1e706181850b515d752
Gene Liverman <gliverma@westga.edu>
2015-04-23 21:13:38 -0400

    Ruby-ify matching, move command execution inside confine.

--- lib/facter/nsrports.rb
+++ lib/facter/nsrports.rb
@@ -1,30 +1,23 @@
 require 'facter'
 
-if `pgrep nsrexecd` != ""
-  Facter.add(:nsr_serviceports) do
-    confine :kernel => [ 'Linux', ]
-    setcode do
-      Facter::Util::Resolution.exec("/usr/bin/nsrports | grep Service | sed -e 's/Service Ports: //i' -e's/[[:space:]]*$//'")
-    end
-  end
-  Facter.add(:nsr_connectionports) do
-    confine :kernel => [ 'Linux', ]
-    setcode do
-      Facter::Util::Resolution.exec("/usr/bin/nsrports | grep Connection | sed -e 's/Connection Ports: //i' -e's/[[:space:]]*$//'")
-    end
-  end
-else
-  Facter.add(:nsr_serviceports) do
-    confine :kernel => [ 'Linux', ]
-    setcode do
-      'nsrexecd not running'
-    end
-  end
-  Facter.add(:nsr_connectionports) do
-    confine :kernel => [ 'Linux', ]
-    setcode do
+Facter.add(:nsr_serviceports) do
+  confine :kernel => [ 'Linux', ]
+  setcode do
+    if `pgrep nsrexecd` != ""
+      (`/usr/bin/nsrports`).match(/Service ports: ([0-9]+-[0-9]+)/)[1]
+    else
       'nsrexecd not running'
     end
   end
 end
 
+Facter.add(:nsr_connectionports) do
+  confine :kernel => [ 'Linux', ]
+  setcode do
+    if `pgrep nsrexecd` != ""
+      (`/usr/bin/nsrports`).match(/Connection Ports: ([0-9]+-[0-9]+)/)[1]
+    else
+      'nsrexecd not running'
+    end
+  end
+end
