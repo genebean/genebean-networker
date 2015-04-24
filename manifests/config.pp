417e4d24a156fecb65fcb62f24aa815a2e868ce6
Gene Liverman <gliverma@westga.edu>
2015-04-23 21:25:17 -0400

    Wrapped resources in a case statement that detects the running OS.

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,39 +5,50 @@ class networker::config (
   $servers_file         = $::networker::servers_file,
   $servers_file_name    = $::networker::servers_file_name,
   $service_portrange    = $::networker::service_portrange,) {
-  file { '/nsr':
-    ensure => 'directory',
-    before => File['/nsr/res'],
-  }
-
-  file { '/nsr/res':
-    ensure => 'directory',
-    before => File['/nsr/res/servers'],
-  }
-
-  case $servers_file {
-    hiera    : {
-      file { '/nsr/res/servers':
-        ensure  => 'present',
-        content => hiera($servers_file_name),
-        require => Package['lgtoclnt'],
-        notify  => Service['networker'],
+  case $::osfamily {
+    RedHat, Debian : {
+      file { '/nsr':
+        ensure => 'directory',
+        before => File['/nsr/res'],
       }
-    }
 
-    template : {
-      file { '/nsr/res/servers':
-        ensure  => 'present',
-        content => template('networker/servers.erb'),
-        require => Package['lgtoclnt'],
-        notify  => Service['networker'],
+      file { '/nsr/res':
+        ensure => 'directory',
+        before => File['/nsr/res/servers'],
       }
+
+      case $servers_file {
+        hiera    : {
+          file { '/nsr/res/servers':
+            ensure  => 'present',
+            content => hiera($servers_file_name),
+            require => Package['lgtoclnt'],
+            notify  => Service['networker'],
+          }
+        }
+
+        template : {
+          file { '/nsr/res/servers':
+            ensure  => 'present',
+            content => template('networker/servers.erb'),
+            require => Package['lgtoclnt'],
+            notify  => Service['networker'],
+          }
+        }
+
+        default  : {
+          fail("Valid options for 'servers_file' are 'hiera' and 'template'.")
+        }
+      }
+    } # end RedHat
+
+    default        : {
+      fail("${::osfamily} is not yet supported by this module.
+       Please file a bug report if it should be.")
     }
 
-    default  : {
-      fail("Valid options for 'servers_file' are 'hiera' and 'template'.")
-    }
-  }
+  } # end case
+
 
   # Set Portranges
   case $::kernel {
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,10 +1,20 @@
 # Configures the NetWorker service
 class networker::service {
-  service { 'networker':
-    ensure     => 'running',
-    enable     => true,
-    hasrestart => false,
-    provider   => 'redhat',
-    require    => File['/nsr/res/servers'],
-  }
+  case $::osfamily {
+    RedHat, Debian : {
+      service { 'networker':
+        ensure     => 'running',
+        enable     => true,
+        hasrestart => false,
+        provider   => 'redhat',
+        require    => File['/nsr/res/servers'],
+      }
+    } # end RedHat
+
+    default        : {
+      fail("${::osfamily} is not yet supported by this module.
+       Please file a bug report if it should be.")
+    }
+
+  } # end case
 }
